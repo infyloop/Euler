4a75e62bda15aa9356a7e020f4083c68cf758ae8
Shabda Raaj <shabda.raaj@gmail.com>
2011-07-01 18:22:33 +0530

    Alternate version of prblm 1

--- 1.py
+++ 1.py
@@ -1,3 +1,8 @@
+#Version 2
+div_3_5 = lambda x : x%3==0 or  x%5==0
+print sum([el for el in range(1,1000) if div_3_5(el)])
+
+#Version 1
 sum = 0
 for i  in range(1,1000):
   if i % 3 == 0:
@@ -6,4 +11,3 @@ for i  in range(1,1000):
 	sum += i	
 print sum
   
-
